096f7fd6bf90d66e3a98106d7c50dc2ce83cda5f
michaelscoggins <rhythmghosts@gmail.com>
2020-12-01 08:23:18 -0600

    able to receive auth0 token

--- src/server/middleware/index.js
+++ src/server/middleware/index.js
@@ -1,24 +1,26 @@
-const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const jwksRsa = require("jwks-rsa");
+const jwt = require("express-jwt");
 
 const logger = (req, res, next) => {
   console.log(`${req.path} ${new Date().toISOString()}`);
   next();
 };
 
-const authenticate = (req, res, next) => {
-  const header = req.headers["authorization"] || "";
-  const [bearer, token] = header.split(" ");
-
-  try {
-    const decoded = jwt.verify(token, "secret");
-    req.user = decoded;
-    next();
-  } catch (err) {
-    res.sendStatus(401);
-  }
-};
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
+  }),
+  // Validate the audience and the issuer.
+  audience: process.env.AUTH0_IDENTITY,
+  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
+  algorithms: ["RS256"],
+});
 
 module.exports = {
   logger,
-  authenticate,
+  checkJwt,
 };
--- src/server/routers/users.js
+++ src/server/routers/users.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const usersController = require("../controllers/users");
-const { authenticate } = require("../middleware");
+const { checkJwt } = require("../middleware");
 const router = express.Router();
 
 router.get("/", usersController.getAllUsers);
 router.get("/:id", usersController.getUserById);
-router.post("/", usersController.createUser);
+router.post("/", checkJwt, usersController.createUser);
 router.post("/profile", usersController.createUserProfile);
 router.post("/credentials", usersController.createUserCredentials);
-router.put("/:id", usersController.updateUserById);
-router.delete("/:first_name", usersController.deleteUserByFirstName);
+router.put("/:id", checkJwt, usersController.updateUserById);
+router.delete("/:first_name", checkJwt, usersController.deleteUserByFirstName);
 
 module.exports = router;
