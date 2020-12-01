dd4bb08a41ab5380873c30610ebc54a74fa11403
michaelscoggins <rhythmghosts@gmail.com>
2020-12-01 09:14:42 -0600

    checking out this sign-up auth0 function

--- src/server/controllers/auth.js
+++ src/server/controllers/auth.js
@@ -10,22 +10,23 @@ const { handleSQLError } = require("../sql/error");
 // for bcrypt
 const saltRounds = 10;
 
-// const signup = (req, res) => {
-//   const { username, password } = req.body
-//   let sql = "INSERT INTO usersCredentials (username, password) VALUES (?, ?)"
+const signup = (req, res) => {
+  const { username, password } = req.body;
+  let sql = "INSERT INTO usersCredentials (username, password) VALUES (?, ?)";
 
-//   bcrypt.hash(password, saltRounds, function(err, hash) {
-//     sql = mysql.format(sql, [ username, hash ])
+  bcrypt.hash(password, saltRounds, function (err, hash) {
+    sql = mysql.format(sql, [username, hash]);
 
-//     pool.query(sql, (err, result) => {
-//       if (err) {
-//         if (err.code === 'ER_DUP_ENTRY') return res.status(409).send('Username is taken')
-//         return handleSQLError(res, err)
-//       }
-//       return res.send('Sign-up successful')
-//     })
-//   })
-// }
+    pool.query(sql, (err, result) => {
+      if (err) {
+        if (err.code === "ER_DUP_ENTRY")
+          return res.status(409).send("Username is taken");
+        return handleSQLError(res, err);
+      }
+      return res.send("Sign-up successful");
+    });
+  });
+};
 
 const login = (req, res) => {
   const { username, password } = req.body;
@@ -54,31 +55,30 @@ const login = (req, res) => {
       console.log(e);
     });
 
-  // let sql = "SELECT * FROM usersCredentials WHERE username = ?"
-  // sql = mysql.format(sql, [ username ])
+  let sql = "SELECT * FROM usersCredentials WHERE username = ?";
+  sql = mysql.format(sql, [username]);
 
-  // pool.query(sql, (err, rows) => {
-  //   if (err) return handleSQLError(res, err)
-  //   if (!rows.length) return res.status(404).send('No matching users')
+  pool.query(sql, (err, rows) => {
+    if (err) return handleSQLError(res, err);
+    if (!rows.length) return res.status(404).send("No matching users");
 
-  //   const hash = rows[0].password
-  //   bcrypt.compare(password, hash)
-  //     .then(result => {
-  //       if (!result) return res.status(400).send('Invalid password')
+    const hash = rows[0].password;
+    bcrypt.compare(password, hash).then((result) => {
+      if (!result) return res.status(400).send("Invalid password");
 
-  //       const data = { ...rows[0] }
-  //       data.password = 'REDACTED'
+      const data = { ...rows[0] };
+      data.password = "REDACTED";
 
-  //       const token = jwt.sign(data, 'secret')
-  //       res.json({
-  //         msg: 'Login successful',
-  //         token
-  //       })
-  //     })
-  // })
+      const token = jwt.sign(data, "secret");
+      res.json({
+        msg: "Login successful",
+        token,
+      });
+    });
+  });
 };
 
 module.exports = {
-  // signup,
+  signup,
   login,
 };
--- src/server/routers/auth.js
+++ src/server/routers/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const authController = require("../controllers/auth");
 const router = express.Router();
 
-// router.post("/signup", authController.signup);
+router.post("/signup", authController.signup);
 router.post("/login", authController.login);
 
 module.exports = router;
